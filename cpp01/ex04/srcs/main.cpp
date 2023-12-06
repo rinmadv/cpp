871af485d5a18918e6b5e3d303f1cbfe7258e346
madavid@student.42.fr <madavid@student.42.fr>
2023-12-06 16:37:36 +0100

    cpp01 ex 04 in progress

---
+++ cpp01/ex04/srcs/main.cpp
@@ -0,0 +1,17 @@
+#include "../includes/nom.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cerr << argv[0] << " [infile] [string1] [string2]" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::ifstream infile(argv[1])
+	if ()
+	
+	return (EXIT_SUCCESS);
+}
