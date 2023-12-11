b3ce9680e2cf76f3010303bf06863e8f33e8ff29
rinmadv <marine.mardavid@gmail.com>
2023-12-11 18:58:30 +0100

    cpp03 ex01 done

---
+++ cpp03/ex01/srcs/ScavTrap.cpp
@@ -0,0 +1,25 @@
+#include "../includes/ScavTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {std::cout << "Scav Trap constructor get called for " << name << std::endl;}
+ScavTrap::ScavTrap(ScavTrap const & src)
+{
+	*this = src;
+	std::cout << "Scav trap copy constructor get called" << std::endl;
+}
+
+ScavTrap::~ScavTrap(){std::cout << "Scav trap destructor get called" << std::endl;};
+
+ScavTrap & ScavTrap::operator=(ScavTrap const & rhs)
+{
+	std::cout << "Scav trap assignation operator get called" << std::endl;
+	this->_attackDamage = rhs._attackDamage;
+	this->_energy = rhs._energy;
+	this->_hit = rhs._hit;
+	this->_name = rhs._name;
+	return (*this);
+}
+
+void	ScavTrap::guardGate()
+{
+	std::cout << "Scav trap is now in Gate keeper mode" << std::endl;
+}
---
+++ cpp03/ex01/srcs/main.cpp
@@ -0,0 +1,22 @@
+#include "../includes/ScavTrap.hpp"
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	ClapTrap David("David");
+	ClapTrap Goliath("Goliath");
+	ScavTrap Getta("Getta");
+
+	David.attack("Goliath");
+	Goliath.takeDamage(David.getAttackDamageval());
+	Goliath.beRepaired(1000);
+	Goliath.beRepaired(2);
+	Getta.attack("David");
+	David.takeDamage(Getta.getAttackDamageval());
+	David.beRepaired(UINT_MAX);
+	for (int i = 0; i <= 50; i++)
+		David.beRepaired(1);
+	Getta.guardGate();
+	return 0;
+}
