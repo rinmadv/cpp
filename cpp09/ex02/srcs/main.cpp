9fe635250eb6f2c0b4a554c9446457b1edd4f830
madavid@student.42.fr <madavid@student.42.fr>
2024-09-16 10:44:13 +0200

    dont know what i did

--- cpp09/ex02/srcs/main.cpp
+++ cpp09/ex02/srcs/main.cpp
@@ -85,8 +85,8 @@ bool	removeB(std::vector<int> &mainChain, std::vector<int> &bChain, size_t group
 		std::vector<int>::iterator start = mainChain.begin() + offset + i;
 		if (start + groupsize + 1 > mainChain.end())//pas sure du +1 
 		{
-			std::cout << "b chain apres remove b:" << std::endl;
-			displayVector(bChain);
+			// std::cout << "b chain apres remove b:" << std::endl;
+			// displayVector(bChain);
 			return (false);
 		}
 		std::vector<int>::iterator end = start + groupsize;
@@ -117,6 +117,31 @@ void	removeLeftovers(std::vector<int> &mainChain, std::vector<int> &bChain, size
 	displayVector(bChain);
 }
 
+// std::vector<int>::iterator custom_lower_bound(std::vector<int>::iterator first, std::vector<int>::iterator last, int value, size_t nbGroups, size_t groupeSize)
+// {
+// 	std::cout << _PINK << "Premiere comp " << *first << ", seconde comp " << *last << " reference :" << value << std::endl << _END;
+// 	// size_t groupNBmid;
+// 	while (nbGroups != 2 && nbGroups != 0)
+// 	{
+// 		std::cout << _LAGOON << "On a " << nbGroups;
+// 		nbGroups /= 2; 
+// 		std::vector<int>::iterator mid = first + nbGroups * groupeSize;
+// 		std::cout  << " et le milieu c'est " << *mid << _END << std::endl;
+// 	}
+	
+	
+// 	(void) nbGroups;
+// 	(void) groupeSize;
+// 	return (first); //a changer
+// }
+
+std::vector<int>::iterator	binarySearch(std::vector<int> vec, int toFind)
+{
+	size_t sizeVec = vec.size();
+
+	
+}
+
 void	insertBChain(std::vector<int> &mainChain, std::vector<int> &bChain, size_t groupsize)
 {
 	size_t bInsertionsNb = bChain.size() / groupsize;
@@ -126,24 +151,49 @@ void	insertBChain(std::vector<int> &mainChain, std::vector<int> &bChain, size_t 
 	std::vector<int>::iterator bChainSart = bChain.begin();
 	//maybe faut checker quelque chose entre les deux 
 	std::vector<int>::iterator bChainEnd = bChainSart + groupsize;
-
+	size_t insertionCount = 2;
+	size_t nbGroupInMain = 2;
+	std::vector<int>::iterator mainChainStart = mainChain.begin() + groupsize - 1;
+	std::vector<int>::iterator mainChainEnd;
 
 	// std::vector<int>::iterator mainChainEnd = mainChainSart * ;
 	// std::vector<int>::iterator mainChainSart = mainChain.begin() + groupsize;
 	// int = 0;
-
+	// std::lower_bound(mainChainStart, )
 	while (bChainEnd <= bChain.end())
 	{
+		std::cout << _SALMON "On insrere b" << insertionCount  << "(" << *(bChainEnd-1) << ") dans " << nbGroupInMain << " groupes de taille " << groupsize << std::endl;
+		//  *bChainSart << " a " << *bChainEnd << std::endl; //surement invalid read ici (check avec vg) mais juste sur l'affichage ecran
+		
+
 		/* Pour apres quand on aura fait l'insertion*/
-		std::cout << "On supprime de : " << *bChainSart << " a " << *bChainEnd << std::endl; //surement invalid read ici (check avec vg) mais juste sur l'affichage ecran
+		// std::cout << "On add (later supprime) de : " << *bChainSart << " a " << *(bChainEnd - 1) << std::endl << _END; //surement invalid read ici (check avec vg) mais juste sur l'affichage ecran
+		mainChainEnd = mainChainStart + (groupsize * nbGroupInMain);
+		// std::cout << _MAGENTA << "Premier chiffre compare: " << *mainChainStart << " vs dernier chiffre comparé " << *mainChainEnd << std::endl << _END;
+		// std::cout << _RED << "on insere " <<  *(bChainEnd-1) << " après " << *custom_lower_bound(mainChainStart, mainChainEnd, *(bChainEnd-1), nbGroupInMain, groupsize) << _END << std::endl;
+		// *custom_lower_bound(mainChainStart, mainChainEnd - groupsize, *(bChainEnd - 1), nbGroupInMain, groupsize);
+		
+		std::vector<int> compared;
+		for (std::vector<int>::iterator it = mainChainStart ; *it != *mainChainEnd; it += groupsize)
+		{
+			compared.push_back(*it);
+		}
+		std::cout << _RED "tableau comparés : ";
+		displayVector(compared);
+		std::cout << std::endl << _END;
+		std::vector<int>::iterator insertAt = binarySearch(compared, toFind);
+		std::cout << _PURPLE << *place << _END << std::endl;
+		
 		bChain.erase(bChainSart, bChainEnd);
 		std::cout << _ORANGE << "New bChain : " _END;
 		displayVector(bChain);
 		// //MAJ les iterateurs
+		insertionCount += 1;
+		nbGroupInMain += 2;
 	}
 	
 
-	if (leftoversInsertionsNb)
+	if (leftoversInsertionsNb)//attention si cest level 0 (mais normalement en level 0 yaura plus de reste)
 	{
 		mainChain.insert(mainChain.end() - 1, bChain.begin(), bChain.end());
 		bChain.erase(bChain.begin(), bChain.end()); //maybe je peux utiliser clear, maisa a voir ce que fait la fonction
@@ -167,16 +217,16 @@ void	ford_johnson(std::vector<int> &vec, int exp)
 	/*If we only have one element, comparisirion is not possible and thus, this element is considered as sorted and recursion stops*/
 	if (nextStep > size)
 		return;
-	std::cout << _BOLD _CYAN "Recursion level " << exp << std::endl << _END;
+	// std::cout << _BOLD _CYAN "Recursion level " << exp << std::endl << _END;
 	swapPairs(vec, step, nextStep);
-	displayVector(vec);
+	// displayVector(vec);
 	ford_johnson(vec, exp + 1);
 	std::vector<int> bChain;
 	std::cout << _BOLD _CYAN "Back to recursion level " << exp << std::endl << _END;
-	std::cout << "vec avant remove : ";
-	displayVector(vec);
-	std::cout << "bchain avant remove : ";
-	displayVector(bChain);
+	// std::cout << "vec avant remove : ";
+	// displayVector(vec);
+	// std::cout << "bchain avant remove : ";
+	// displayVector(bChain);
 	
 	if (removeB(vec, bChain, step))
 	{
@@ -196,6 +246,7 @@ void	ford_johnson(std::vector<int> &vec, int exp)
 	displayVector(vec);
 	std::cout << "bchain apres insertion : ";
 	displayVector(bChain);
+	std::cout << std::endl << std::endl;
 }
 
 int main(int argc, char **argv)
@@ -203,7 +254,7 @@ int main(int argc, char **argv)
 	if (argc == 1)
 		return (displayMessage("No number"), EXIT_FAILURE);
 
-	//Parsing, mettre les arg dans un vecteur (a mettre dans une fonction + faire les verif)
+	//Parsing, mettre les arg dans un vecteur (a mettre dans une fonction + faire les verif doublons)
 	std::vector<int> vec;
 	for (int i = 1; i < argc; i++)
 	{
